e96dd482ed87f68fd44be9cf1180c0ed3913d200
kshitij saini <kshitizsaini1941996@gmail.com>
2022-08-02 10:03:49 +0530

    eComm_2 Added Basic Automated Testing

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello')
+  try {
+    res.status(200).send('Hello')
+  } catch (error) {
+    console.log(`Internal Server Error ${error}`)
+    res.status(500).send(`Internal Server Error ${error}`)
+  }
 })
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
+
+module.exports = app
